0189f4e588c05d695602ad02c9784892a5558139
hotdari <38778044+hotdari@users.noreply.github.com>
2021-03-07 11:15:34 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -10,6 +11,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// url 통해서 전달되는 빈공간, 한글이 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,43 @@
-const home = (req, res) => {
-  res.render("home/index");
-};
+"use strict";
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["admin", "관리자", "김팀장"],
+  password: ["admin", "1234", "123456"]
+}
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  }
+}
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if(users.password[idx] === password){
+        return res.json({
+          success: true
+        })
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다."
+    })
+  }
+
+  
 }
 
 module.exports = {
-  home,
-  login
+  output,
+  process
 }
