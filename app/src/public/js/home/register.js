3068a537f7e1e34813b6eba55ec2998634554f12
hotdari <38778044+hotdari@users.noreply.github.com>
2021-03-20 23:53:40 +0900

    라우터 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,14 +9,14 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register(){
+  if(!id.value) return alert("아이디를 입력해주세요.");
+  if(password.value !== confirmPassword.value) return alert("비밀번호가 일치하지 않습니다.");
+
   const req = {
     id : id.value,
     name : username.value,
     password : password.value,
-    confirmPassword : confirmPassword.value
   };
-
-  console.log(req);
 
   fetch('/register', {
     method: "POST",
