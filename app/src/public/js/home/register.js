5c5881d486c414a049b8f9f10976ade152a457c8
hotdari <38778044+hotdari@users.noreply.github.com>
2021-03-20 22:47:34 +0900

    회원가입 페이지 작성

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  username = document.querySelector("#name"),
+  password = document.querySelector("#password"),
+  confirmPassword = document.querySelector("#confirm-password"),
+  registerBtn = document.querySelector("#button")
+
+registerBtn.addEventListener("click", register);
+
+function register(){
+  const req = {
+    id : id.value,
+    name : username.value,
+    password : password.value,
+    confirmPassword : confirmPassword.value
+  };
+
+  console.log(req);
+
+  fetch('/register', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req)
+  })
+  .then(res => res.json())
+  .then(res => {
+    if(res.success){
+      location.href = "/login";
+    } else {
+      alert(res.msg);
+    }
+  })
+  .catch(err => {
+    console.error("회원가입 중 에러 발생");
+  })
+}
