5e35174c2ad2145fa4c4847c0153e13e719daf63
hotdari <38778044+hotdari@users.noreply.github.com>
2021-03-22 00:22:59 +0900

    파일시스템을 통한 로그인 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,22 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = {
-    id: ["admin", "관리자", "김팀장"],
-    password: ["admin", "1234", "123456"],
-    name: ["홍길동", "테스터", "김테스트"]
+  static #getUserInfo = (data, id) => {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
   }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if(users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -19,19 +27,15 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id)
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
+    return fs.readFile("./src/databases/users.json")
+      .then(data => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);  
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
